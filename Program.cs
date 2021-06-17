990db9996e277b87ba6ff12b5acad3d9d4eee69c
Георгий <gera.sukhomlin@gmail.com>
2021-06-17 19:18:44 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Composite_querries_via_SQLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
